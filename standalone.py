17a524c87c0cd7c16839ecdc128d3fade1ad6727
Gergely Wootsch <hello@gergely-wootsch.com>
2018-11-13 11:56:47 +0100

    standalone tweaks

--- standalone.py
+++ standalone.py
@@ -1,11 +1,17 @@
 """Standalone runner."""
 
 import sys
+import ctypes
 from PySide2 import QtWidgets, QtGui, QtCore
 from mayabrowser.toolbar import MayaBrowserWidget
 
+
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
+    app.setApplicationName('Browser')
+    appID = u'glassworks.browser' # arbitrary string
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(appID)
+
     widget = MayaBrowserWidget()
     widget.move(50, 50)
     widget.show()
