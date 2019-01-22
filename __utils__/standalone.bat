6d63697846ad27d09c050ec10cc82ae5762c6db5
Gergely Wootsch <hello@gergely-wootsch.com>
2019-01-22 16:57:51 +0100

    moved superflous modules here

---
+++ __utils__/standalone.bat
@@ -0,0 +1,34 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdDPlH1aW8E0Mex9rXwyDLm6GC7QF6dT84+uUsVoSRuksRJzYyL2eNPMv50T9dJQ/2W1JndkwGBNNcxClbwY4ulJWuWuJPsSTvV20HHeK9GIPEmZ7iS3ZjyRb
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdF25
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJgZkoaHGQ=
+::ZQ05rAF9IBncCkqN+0xwdVsEAlXMbCXqZg==
+::ZQ05rAF9IAHYFVzEqQISOhRDXwGQXA==
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDCaQM3ypA6d8
+::cRolqwZ3JBvQF1fEqQIAIQtRQA2MOSuvErwQ4fzir+yIsA0fRu0oa4bV3fSeMu8Z70bwZvY=
+::dhA7uBVwLU+EWH6B5kU1JAIUewuNKHi5DtU=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdDPlH1aW8E0Mex9rXwyDLm6GC7QF6dT84+uUsVoSRuksRJzYyL2eNPMv50T9dJQ/2W1JndkwDglXaQGufEExsWsi
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+pythonw "\\gordo\3d_share\maya\glassworks\scripts\mayabrowser\standalone.py"
