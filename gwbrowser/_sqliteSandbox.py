2f94a2f7e2f00fa66ed9042633d444662be94a63
freelance8 <freelance8@gwbar.local>
2020-02-04 19:05:44 +0100

    wip

--- gwbrowser/_sqliteSandbox.py
+++ gwbrowser/_sqliteSandbox.py
@@ -2,15 +2,19 @@
 I'm hoping this will result in some much needed performance increase, especially
 querrying file flags and annotations."""
 
+# import hashlib
+import gwbrowser.common as common
+
 import sqlite3
 from sqlite3 import Error
 
 DATABASE = ur'C:/tmp/sandbox.db'
 # DATABASE = ur':memory:'
 
-
-# Description
-# TaskList
+# path -- This could either be a
+# [Hash] -- We no longer need to hash the filenames
+# description
+# tasks
 # Flags
 # filepath
 # Thumbnail path
@@ -19,7 +23,7 @@ DATABASE = ur'C:/tmp/sandbox.db'
 # User
 
 
-class DBConnector(object):
+class AssetSettings(object):
     """Test connector class"""
 
     def __init__(self, database_path):
@@ -44,12 +48,44 @@ class DBConnector(object):
     def cursor(self):
         if not self.isValid():
             return None
-
         return self.connection().cursor()
 
-conn = DBConnector(DATABASE)
+    def create_table(self):
+        if not self.isValid():
+            return
+
+        with self.connection() as conn:
+            conn.cursor().execute("""
+                CREATE TABLE IF NOT EXISTS data (
+                    id TEXT PRIMARY_KEY,
+                    filepath TEXT,
+                    description TEXT,
+                    notes TEXT,
+                    flags INTEGER,
+                    thumbnail_path TEXT,
+                    thumbnail_timestamp REAL,
+                    thumbnail_hash TEXT,
+                    last_user TEXT
+                )""")
+            # conn.cursor().execute("""PRAGMA encoding='UTF-8';""")
+            # conn.commit()
+
+    @staticmethod
+    def hash(server, job, root, filepath):
+        # Sequences have their own asset setting and because the sequence frames might
+        # change we will use a generic name instead of the current in-out frames
+        collapsed = common.is_collapsed(filepath)
+        if collapsed:
+            filepath = collapsed.expand(ur'\1[0]\3')
+        path = filepath.replace(server, u'').strip(u'/')
+        path = hashlib.md5(path.encode(u'utf-8')).hexdigest()
+        return path
+
+
+conn=AssetSettings(DATABASE)
+
+
 if not conn.isValid():
     print conn.last_error()
 
-print conn.cursor()
-# print connector.connection
+conn.create_table()
