4f85d88c24fad10691f24fa224df15266a4a6374
freelance8 <freelance8@gwbar.local>
2020-02-05 19:30:08 +0100

    Database work in progress

--- gwbrowser/_sqliteSandbox.py
+++ gwbrowser/_sqliteSandbox.py
@@ -2,9 +2,11 @@
 I'm hoping this will result in some much needed performance increase, especially
 querrying file flags and annotations."""
 
+from contextlib import contextmanager
 import gwbrowser.common as common
 from PySide2 import QtCore
-import hashlib
+import time
+import platform
 import sqlite3
 from sqlite3 import Error
 
@@ -34,20 +36,20 @@ class BookmarkDatabase(object):
 
         # Let's make sure the parent folder is created first, otherwise
         # we won't be able to create the database file.
-        if not QtCore.QFileInfo(database_path).dir().mkpath('.'):
-            self._exception = u'Unable to create database dir\n"{}"'.format(database_path)
+        if not QtCore.QFileInfo(database_path).dir().mkpath(u'.'):
+            self._exception = u'Unable to create database dir\n"{}"'.format(
+                database_path)
             return
 
         try:
-            self._connection = sqlite3.connect(database_path, isolation_level=u'DEFERRED')
+            self._connection = sqlite3.connect(
+                database_path, isolation_level=None)
+            self._connection.execute(u'pragma journal_mode=wal;')
+            self._connection.execute(u'pragma synchronous=OFF;')
         except Error as e:
             self._exception = u'Unable to connect to the database at "{}"\n-> "{}"'.format(
                 database_path, e.message)
             return
-
-    def value(self, path, key):
-        with self.connection() as conn:
-            conn.execute()
 
     def _get_id(self, filepath):
         collapsed = common.is_collapsed(filepath)
@@ -56,7 +58,93 @@ class BookmarkDatabase(object):
         filepath = filepath.lower().replace(self._server, u'').strip(u'/')
         return filepath
 
-    def setValue(self, path, key, value, commit=False):
+    @contextmanager
+    def transaction_contextmanager(self):
+        """Simple context manager for controlling transactions.
+        We're explicitly calling `BEGIN` before the `execute()`. We also roll
+        changes back if an error has been encountered.
+
+        """
+        self._connection.execute('BEGIN')
+        try:
+            yield
+        except:
+            self._connection.rollback()
+            raise
+        else:
+            self._connection.commit()
+
+    def connection(self):
+        return self._connection
+
+    def isValid(self):
+        return True if self._connection else False
+
+    def last_error(self):
+        return self._exception
+
+    def init_tables(self):
+        """Initialises the database with the default tables.
+
+        The  `data` table stores file information of items inside the bookmark,
+        whilst `info` contains information about the database itself.
+
+        """
+        if not self.isValid():
+            return
+        with self.transaction_contextmanager():
+            # Main ``data`` table
+            self._connection.cursor().execute("""
+                CREATE TABLE IF NOT EXISTS data (
+                    path TEXT PRIMARY KEY COLLATE NOCASE,
+                    description TEXT,
+                    notes TEXT,
+                    flags INTEGER,
+                    thumbnail_path TEXT,
+                    thumbnail_timestamp REAL,
+                    thumbnail_hash TEXT,
+                    user TEXT
+                )
+            """)
+            # Single-row ``info`` table
+            self._connection.cursor().execute("""
+                CREATE TABLE IF NOT EXISTS info (
+                    path TEXT PRIMARY KEY COLLATE NOCASE,
+                    server TEXT NOT NULL,
+                    job TEXT NOT NULL,
+                    root TEXT NOT NULL,
+                    user TEXT NOT NULL,
+                    host TEXT NOT NULL,
+                    created REAL NOT NULL
+                )
+            """)
+            # Adding info data to the ``info`` table
+            self._connection.execute("""
+            INSERT OR IGNORE INTO info
+                (path, server, job, root, user, host, created)
+            VALUES
+                ('{path}', '{server}', '{job}', '{root}', '{user}', '{host}', '{created}')
+            """.format(
+                path=self._bookmark,
+                server=self._server,
+                job=self._job,
+                root=self._root,
+                user=common.get_username(),
+                host=platform.node(),
+                created=time.time(),
+            ))
+
+    def value(self, path, key):
+        cursor = self._connection.cursor()
+        cursor.execute("""SELECT {key} FROM data WHERE path='{path}'""".format(
+            key=key,
+            path=self._get_id(path)))
+        # cursor.execute("""SELECT description FROM data WHERE path='job/root/testfolder/testfile.ma1'""".format(
+        #     key='description'))
+        # cursor.execute('BEGIN')
+        print cursor.fetchall()
+
+    def setValue(self, path, key, value):
         """Sets a value to the database.
 
         Pass the full the file or folder path including the server, job and root. The database uses the relative path as the row id, which is
@@ -64,7 +152,7 @@ class BookmarkDatabase(object):
         a new one if the row id does not exists yet.
 
         Note:
-            The method does NOT commit the transaction by default.
+            The method does NOT commit the transaction!
 
         Args:
             path (unicode): A file path.
@@ -83,57 +171,24 @@ class BookmarkDatabase(object):
             key=key,
             value=value
         ))
-        if commit:
-            self._connection.commit()
-
-    def connection(self):
-        return self._connection
-
-    def isValid(self):
-        return True if self._connection else False
-
-    def last_error(self):
-        return self._exception
-
-    def cursor(self):
-        if not self.isValid():
-            return None
-        return self.connection().cursor()
-
-    def create_table(self):
-        if not self.isValid():
-            return
-
-        with self.connection() as conn:
-            conn.cursor().execute("""
-                CREATE TABLE IF NOT EXISTS data (
-                    path TEXT PRIMARY KEY COLLATE NOCASE,
-                    description TEXT,
-                    notes TEXT,
-                    flags INTEGER,
-                    thumbnail_path TEXT,
-                    thumbnail_timestamp REAL,
-                    thumbnail_hash TEXT,
-                    last_user TEXT
-                )""")
-            conn.commit()
 
 
+if __name__ == '__main__':
+    bookmark_db = BookmarkDatabase(
+        index=QtCore.QModelIndex(),
+        server='C:/tmp',
+        job='job',
+        root='root',
+    )
+    if not bookmark_db.isValid():
+        print bookmark_db.last_error()
+    bookmark_db.init_tables()
 
-bookmark_db = BookmarkDatabase(
-    index=QtCore.QModelIndex(),
-    server='C:/tmp',
-    job='job',
-    root='root',
-)
-
-if not bookmark_db.isValid():
-    print bookmark_db.last_error()
-
-bookmark_db.create_table()
-
-
-for n in xrange(200000):
-    bookmark_db.setValue(ur'C:/tmp/job/root/testfolder/testfile.ma{}'.format(n * 2), 'description', 'test description!!')
-    bookmark_db.setValue(ur'C:/tmp/job/root/testfolder/testfile.ma{}'.format(n * 2), 'notes', 'test note')
-bookmark_db.connection().commit()
+    with bookmark_db.transaction_contextmanager():
+        for n in xrange(1000):
+            bookmark_db.setValue(
+                ur'C:/tmp/job/root/testfolder/testfile.ma{}'.format(n), u'description', u'test description{}'.format(n))
+            bookmark_db.setValue(
+                ur'C:/tmp/job/root/testfolder/testfile.ma{}'.format(n), u'notes', u'test note')
+    # with bookmark_db.transaction_contextmanager():
+    bookmark_db.value(ur'C:/tmp/job/root/testfolder/testfile.ma123', u'description')
