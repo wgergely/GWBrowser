42d2e90b76d66b8fbcf76162f33b6a5d6d7e97cf
Gergely Wootsch <hello@gergely-wootsch.com>
2020-02-05 08:57:23 +0100

    Keep investigating SQLite databases and port AssetSettings
    I'm excited about this, already seeing a big performance boost using
    databases. I reckon the way AssetSettings will function will differ from
    the current implementation. One big benefit I can see is that the
    `AssetSettings` instances can be replaced with a single
    `BookmarkDatabase` instance. This will have to be linked to each saved
    bookmark, perhaps by storing it inside the BookmarksModel.
    
    I read that it is wise to keep databases small, and although I could
    create one single database in the root of the job, and crate tables per
    bookmark, I will go ahead and implement separate databases per bookmark.
    They will still be stored in the root of the bookmark folder.

--- gwbrowser/_sqliteSandbox.py
+++ gwbrowser/_sqliteSandbox.py
@@ -2,39 +2,89 @@
 I'm hoping this will result in some much needed performance increase, especially
 querrying file flags and annotations."""
 
-# import hashlib
 import gwbrowser.common as common
-
+from PySide2 import QtCore
+import hashlib
 import sqlite3
 from sqlite3 import Error
 
-DATABASE = ur'C:/tmp/sandbox.db'
-# DATABASE = ur':memory:'
 
-# path -- This could either be a
-# [Hash] -- We no longer need to hash the filenames
-# description
-# tasks
-# Flags
-# filepath
-# Thumbnail path
-# Thumbnail cache path
-# Thumbnail cache timestamp
-# User
-
-
-class AssetSettings(object):
-    """Test connector class"""
-
-    def __init__(self, database_path):
+class BookmarkDatabase(object):
+    def __init__(self, index=QtCore.QModelIndex(), server=None, job=None, root=None, parent=None):
         self._connection = None
         self._exception = u''
 
+        if index.isValid():
+            parents = index.data(common.ParentPathRole)
+            if not parents:
+                raise RuntimeError(
+                    u'Index does not contain a valid parent path information')
+            server, job, root = parents[0:3]
+
+        self._server = server.lower()
+        self._job = job.lower()
+        self._root = root.lower()
+        self._bookmark = u'{}/{}/{}'.format(server, job, root).lower()
+
+        database_path = u'{server}/{job}/{root}/.browser/bookmark.db'.format(
+            server=self._server,
+            job=self._job,
+            root=self._root
+        )
+
+        # Let's make sure the parent folder is created first, otherwise
+        # we won't be able to create the database file.
+        if not QtCore.QFileInfo(database_path).dir().mkpath('.'):
+            self._exception = u'Unable to create database dir\n"{}"'.format(database_path)
+            return
+
         try:
-            self._connection = sqlite3.connect(DATABASE)
+            self._connection = sqlite3.connect(database_path, isolation_level=u'DEFERRED')
         except Error as e:
-            self._exception = u'# ERROR: Unable to open connection to the "{}" database file:\n-> "{}"'.format(
+            self._exception = u'Unable to connect to the database at "{}"\n-> "{}"'.format(
                 database_path, e.message)
+            return
+
+    def value(self, path, key):
+        with self.connection() as conn:
+            conn.execute()
+
+    def _get_id(self, filepath):
+        collapsed = common.is_collapsed(filepath)
+        if collapsed:
+            filepath = collapsed.expand(ur'\1[0]\3')
+        filepath = filepath.lower().replace(self._server, u'').strip(u'/')
+        return filepath
+
+    def setValue(self, path, key, value, commit=False):
+        """Sets a value to the database.
+
+        Pass the full the file or folder path including the server, job and root. The database uses the relative path as the row id, which is
+        returned by `_get_id()`. The method will update existing row, or create
+        a new one if the row id does not exists yet.
+
+        Note:
+            The method does NOT commit the transaction by default.
+
+        Args:
+            path (unicode): A file path.
+            key (unicode): A database column name.
+            value (unicode or float): The value to set.
+
+        """
+        if not self.isValid():
+            return
+
+        self._connection.execute("""
+        INSERT INTO data(path, {key}) VALUES('{path}','{value}')
+          ON CONFLICT(path) DO UPDATE SET {key}=excluded.{key};
+        """.format(
+            path=self._get_id(path),
+            key=key,
+            value=value
+        ))
+        if commit:
+            self._connection.commit()
 
     def connection(self):
         return self._connection
@@ -57,8 +107,7 @@ class AssetSettings(object):
         with self.connection() as conn:
             conn.cursor().execute("""
                 CREATE TABLE IF NOT EXISTS data (
-                    id TEXT PRIMARY_KEY,
-                    filepath TEXT,
+                    path TEXT PRIMARY KEY COLLATE NOCASE,
                     description TEXT,
                     notes TEXT,
                     flags INTEGER,
@@ -67,25 +116,24 @@ class AssetSettings(object):
                     thumbnail_hash TEXT,
                     last_user TEXT
                 )""")
-            # conn.cursor().execute("""PRAGMA encoding='UTF-8';""")
-            # conn.commit()
-
-    @staticmethod
-    def hash(server, job, root, filepath):
-        # Sequences have their own asset setting and because the sequence frames might
-        # change we will use a generic name instead of the current in-out frames
-        collapsed = common.is_collapsed(filepath)
-        if collapsed:
-            filepath = collapsed.expand(ur'\1[0]\3')
-        path = filepath.replace(server, u'').strip(u'/')
-        path = hashlib.md5(path.encode(u'utf-8')).hexdigest()
-        return path
+            conn.commit()
 
 
-conn=AssetSettings(DATABASE)
+
+bookmark_db = BookmarkDatabase(
+    index=QtCore.QModelIndex(),
+    server='C:/tmp',
+    job='job',
+    root='root',
+)
+
+if not bookmark_db.isValid():
+    print bookmark_db.last_error()
+
+bookmark_db.create_table()
 
 
-if not conn.isValid():
-    print conn.last_error()
-
-conn.create_table()
+for n in xrange(200000):
+    bookmark_db.setValue(ur'C:/tmp/job/root/testfolder/testfile.ma{}'.format(n * 2), 'description', 'test description!!')
+    bookmark_db.setValue(ur'C:/tmp/job/root/testfolder/testfile.ma{}'.format(n * 2), 'notes', 'test note')
+bookmark_db.connection().commit()
