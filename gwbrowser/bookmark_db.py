34e609fc828fabda7f065ed99a9f24ea1609fada
Gergely Wootsch <hello@gergely-wootsch.com>
2020-02-20 09:59:14 +0100

    Fix/path database implementation
    This is probably the first seems-to-be-working take on the SQLite
    database implementation. There are obvious problems, eg. database
    connection creating seems a unpredictably fail and multithreading
    seems also to be a problem-child. However, it works!

--- gwbrowser/bookmark_db.py
+++ gwbrowser/bookmark_db.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 """SQLite based database used to store file information associated with
 a bookmark.
 
@@ -18,11 +20,11 @@ SERVER_TABLE_KEYS = (u'server', u'job', u'root', u'user', u'host')
 DATA_TABLE_KEYS = (u'description', u'notes', u'flags',
                    u'thumbnail_stamp', u'user')
 
-__DB_CONNECTIONS = {}
+_DB_CONNECTIONS = {}
 """We will store our db connection instances here. They will be created per thread."""
 
 
-def get_db(index):
+def get_db(index, server=None, job=None, asset=None):
     """Helper function to return the bookmark database connection associated
     with an index. We will create the connection if it doesn't exists yet.
     Connection instances cannot be shared between threads hence we will
@@ -35,35 +37,39 @@ def get_db(index):
         BookmarkDB: A BookmarkDB instance that lives in the current thread.
 
     """
-    global __DB_CONNECTIONS
+    global _DB_CONNECTIONS
     thread_id = unicode(QtCore.QThread.currentThread())
 
     if not index.isValid():
-        return None
-    args = index.data(common.ParentPathRole)[0:3]
+        if not all((server, job, root)):
+            raise ValueError(u'Must provide valid server, job, and root')
+        args = (server, job, root)
+    else:
+        args = index.data(common.ParentPathRole)[0:3]
+
     k = u'/'.join(args).lower() + thread_id
-    if k not in __DB_CONNECTIONS:
+    if k not in _DB_CONNECTIONS:
         try:
-            __DB_CONNECTIONS[k] = BookmarkDB(*args)
+            _DB_CONNECTIONS[k] = BookmarkDB(*args)
         except RuntimeError:
             return None
-    return __DB_CONNECTIONS[k]
+    return _DB_CONNECTIONS[k]
 
 
 def reset():
-    global __DB_CONNECTIONS
-    for v in __DB_CONNECTIONS.itervalues():
+    global _DB_CONNECTIONS
+    for v in _DB_CONNECTIONS.itervalues():
         v.deleteLater()
-    __DB_CONNECTIONS = {}
+    _DB_CONNECTIONS = {}
 
 
 class BookmarkDB(QtCore.QObject):
     def __init__(self, server, job, root, parent=None):
         super(BookmarkDB, self).__init__(parent=parent)
         self._connection = None
-        self._server = server.lower()
-        self._job = job.lower()
-        self._root = root.lower()
+        self._server = server.lower().encode(u'utf-8')
+        self._job = job.lower().encode(u'utf-8')
+        self._root = root.lower().encode(u'utf-8')
         self._bookmark = server + u'/' + job + u'/' + root
         self._database_path = u'{server}/{job}/{root}/.bookmark/bookmark.db'.format(
             server=self._server,
@@ -79,7 +85,10 @@ class BookmarkDB(QtCore.QObject):
 
         try:
             self._connection = sqlite3.connect(
-                self._database_path, isolation_level=None)
+                self._database_path,
+                isolation_level=None,
+                check_same_thread=False
+            )
             self.init_tables()
 
             # I don't know if this is needed, but we should try to close the
@@ -90,46 +99,39 @@ class BookmarkDB(QtCore.QObject):
             raise RuntimeError(u'Unable to connect to the database at "{}"\n-> "{}"'.format(
                 self._database_path, e.message))
 
-    def id(self, val):
+    def _row_id(self, k):
         """Pass a valid filepath to retrieve database row number of the given
-        file. Sequences won't have their own settings
+        file. This should be a path wihtout any dynamic sequence numbers as
+        these do change over time, for instance when a sequence range increases.
 
         Args:
-            val (unicode):  File path.
+            k (unicode or int):  The key to use to store the data, usually filepath.
 
         Returns:
-            int: The hashed value of `val`
+            int: The hashed value of `val`.
 
         """
-        if isinstance(val, (int, float, long)):
-            return val
-        val = val.lower()
-
-        # Collapsed sequence ranges
-        collapsed = common.is_collapsed(val)
-        if collapsed:
-            val = collapsed.group(1) + u'[0]' + collapsed.group(3)
-
-        val = val.replace(self._server, u'').strip(u'/')
-
-        # I just realised that the buil-in hash function is probably a bad idea
-        # It seems to be platform dependent, but hashlib should be platform
-        # independent:
-        return int(int(hashlib.md5(val).hexdigest(), 16) % (10 ** 32))
+        k = k.lower().encode(u'utf-8')
+        k = k.replace(u'\\'.encode(u'utf-8'), u'/'.encode(u'utf-8'))
+        if self._server in k:
+            k = k[len(self._server):len(k)]
+        k = k.strip(u'/'.encode(u'utf-8'))
+        return k
 
     def thumbnail_path(self, path):
-        filename = unicode(self.id(path)) + u'.' + common.THUMBNAIL_FORMAT
+        filename = hashlib.md5(self._row_id(path)).hexdigest() + u'.' + common.THUMBNAIL_FORMAT
         p = self._bookmark + u'/.bookmark/' + filename
         return p
 
     @contextmanager
-    def transaction_contextmanager(self):
+    def transactions(self):
         """Simple context manager for controlling transactions.
         We're explicitly calling `BEGIN` before the `execute()`. We also roll
-        changes back if an error has been encountered.
+        changes back if an error has been encountered. The transactions are
+        commited when the context manager goes out of scope.
 
         """
-        self._connection.execute('BEGIN')
+        self._connection.execute(u'BEGIN')
         try:
             yield
         except:
@@ -149,14 +151,14 @@ class BookmarkDB(QtCore.QObject):
         whilst `info` contains information about the database itself.
 
         """
-        with self.transaction_contextmanager():
+        with self.transactions():
             # Main ``data`` table
             self._connection.cursor().execute("""
                 CREATE TABLE IF NOT EXISTS data (
-                    id INTEGER PRIMARY KEY NOT NULL,    /* File's relative path is used as the ID*/
+                    id TEXT PRIMARY KEY COLLATE NOCASE,
                     description TEXT,
                     notes TEXT,
-                    flags INTEGER,
+                    flags INTEGER DEFAULT 0,
                     thumbnail_stamp REAL,
                     user TEXT
                 )
@@ -209,21 +211,20 @@ class BookmarkDB(QtCore.QObject):
             data: The requested value or None.
 
         """
-        if not isinstance(id, int):
-            id = self.id(id)
-
+        id = self._row_id(id)
         _cursor = self._connection.cursor()
-        _cursor.execute("""SELECT {key} FROM {table} WHERE id='{id}'""".format(
-            table=table,
-            key=key,
-            id=self.id(id)))
+        _cursor.execute(u'SELECT {key} FROM {table} WHERE id=\'{id}\''.encode('utf-8').format(
+            table=table.encode('utf-8'),
+            key=key.encode('utf-8'),
+            id=id
+        ))
 
         res = _cursor.fetchone()
         if not res:
             return None
         return res[0]
 
-    def values(self, table=u'data'):
+    def values(self, column=u'*', table=u'data'):
         """Returns all values from the `bookmark.db` of the given table.
 
         Args:
@@ -234,32 +235,45 @@ class BookmarkDB(QtCore.QObject):
 
         """
         _cursor = self._connection.cursor()
-        _cursor.execute("""SELECT * FROM {table}""".format(table=table))
+        if column != u'*':
+            _column = u'id,' + column
+        else:
+            _column = column
+        _cursor.execute("""SELECT {column} FROM {table}""".format(
+                column=_column,
+                table=table
+            ))
 
         # Let's wrap the retrevied data into a more pythonic directory
         _data = _cursor.fetchall()
         data = {}
-        for v in _data:
-            data[v[0]] = {
-                u'description': v[1],
-                u'notes': v[2],
-                u'flags': v[3],
-                u'thumbnail_stamp': v[4],
-                u'user': v[5],
-            }
+        if column == u'*':
+            for v in _data:
+                data[v[0].strip()] = {
+                    u'description': v[1],
+                    u'notes': v[2],
+                    u'flags': v[3],
+                    u'thumbnail_stamp': v[4],
+                    u'user': v[5],
+                }
+        else:
+            for v in _data:
+                data[v[0].strip()] = {
+                    column: v[1],
+                }
         return data
 
-    def setValue(self, id, key, value):
+    def setValue(self, id, key, value, table=u'data'):
         """Sets a value to the database.
 
         Pass the full file or folder path including the server, job and root.
         The database uses the relative path as the row id, which is returned by
-        `id()`. The method will update existing row, or create a new one if the
+        `_row_id()`. The method will update existing row, or create a new one if the
         row `id` does not exists yet.
 
         Note:
             The method does NOT commit the transaction! Use the
-            ``transaction_contextmanager`` to issue a BEGIN statement. The
+            ``transactions`` to issue a BEGIN statement. The
             transactions will be commited once the context manager goes out of
             scope.
 
@@ -269,60 +283,64 @@ class BookmarkDB(QtCore.QObject):
             value (unicode or float): The value to set.
 
         """
+        id = self._row_id(id)
         self._connection.execute("""
-        INSERT INTO data(id, {key}) VALUES('{id}','{value}')
-          ON CONFLICT(id) DO UPDATE SET {key}=excluded.{key};
+        INSERT INTO {table} (id, {key})
+        VALUES('{id}','{value}')
+        ON CONFLICT(id) DO UPDATE SET {key}=excluded.{key};
         """.format(
-            id=self.id(id),
+            id=id,
             key=key,
-            value=value
+            value=value,
+            table=table
         ))
 
 
-bookmark_db = BookmarkDB(
-    server=u'C:/tmp',
-    job=u'job',
-    root=u'root',
-)
-# print bookmark_db.thumbnail_path('C:/temp/job/root/testimage.exr')
-# print bookmark_db.thumbnail_path('c:/Temp/job/root/testimage.exr')
+if __name__ == '__main__':
+    def run_benchmark():
+        """The main takeaaway seems to be that `value()` is quicker on smaller
+        databases, but once I have more than 100k+ items values seems to be
+        performaing quicker.
+
+        """
+
+        # bookmark_db.setValue(ur'job/root/testfolder/testfile.ma', u'description', u'test')
+        return
+        x = 100000
+        # #####################
+        t = time.time()
+        with bookmark_db.transactions():
+            for n in xrange(x):
+                id = bookmark_db._row_id(
+                    ur'job/root/testfolder/testfile.ma{}'.format(n))
+                bookmark_db.setValue(id, u'description',
+                                     u'test description' + unicode(n))
+                bookmark_db.setValue(id, u'notes', u'testnote{}' + unicode(n))
+        print '`setValue()` took', time.time() - t, '(x{})'.format(n), '\n'
+        # #####################
+        t = time.time()
+        with bookmark_db.transactions():
+            for n in xrange(x):
+                id = bookmark_db._row_id(
+                    ur'job/root/testfolder/testfile.ma{}' + unicode(n))
+                v = bookmark_db.value(id, u'description')
+        print '`value()` took', time.time() - t, '\n'
+        ######################
+        t = time.time()
+        data = bookmark_db.values()
+        for n in xrange(x):
+            id = bookmark_db._row_id(ur'job/root/testfolder/testfile.ma' + unicode(n))
+            v = data[id]['description']
+        print '`values()` took', time.time() - t, '\n'
 
 
-def run_benchmark():
-    """The main takeaaway seems to be that `value()` is quicker on smaller
-    databases, but once I have more than 100k+ items values seems to be
-    performaing quicker.
-
-    """
-    bookmark_db = BookmarkDB(
-        server=u'C:/tmp',
-        job=u'job',
-        root=u'root',
+    db = BookmarkDB(
+        server=u'C:/temp',
+        job=u'dir1',
+        root=u'added_bookmark',
     )
+    print db.values()
+    print db.values('notes')
 
-    x = 100000
-    # #####################
-    t = time.time()
-    with bookmark_db.transaction_contextmanager():
-        for n in xrange(x):
-            id = bookmark_db.id(
-                ur'job/root/testfolder/testfile.ma{}'.format(n))
-            bookmark_db.setValue(id, u'description',
-                                 u'test description' + unicode(n))
-            bookmark_db.setValue(id, u'notes', u'testnote{}' + unicode(n))
-    print '`setValue()` took', time.time() - t, '(x{})'.format(n), '\n'
-    # #####################
-    t = time.time()
-    with bookmark_db.transaction_contextmanager():
-        for n in xrange(x):
-            id = bookmark_db.id(
-                ur'job/root/testfolder/testfile.ma{}' + unicode(n))
-            v = bookmark_db.value(id, u'description')
-    print '`value()` took', time.time() - t, '\n'
-    ######################
-    t = time.time()
-    data = bookmark_db.values()
-    for n in xrange(x):
-        id = bookmark_db.id(ur'job/root/testfolder/testfile.ma' + unicode(n))
-        v = data[id]['description']
-    print '`values()` took', time.time() - t, '\n'
+    # run_benchmark()
+    # bookmark_db.setValue(ur'job/root/testfolder/testfile.ma', u'description', u'test')
