799f8257514f7c3d9fb2ffab338a2b2f1f60212a
Gergely Wootsch <wgergely@Gergelys-iMac.local>
2020-03-11 23:39:27 +0100

    Add build scripts

---
+++ build-scripts/osx/make-libraries.sh
@@ -0,0 +1,89 @@
+cd ~
+mkdir ./code
+cd ./code
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+brew doctor
+brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/86a44a0a552c673a05f11018459c9f5faae3becc/Formula/python@2.rb
+brew install
+brew install boost
+brew install boost-python
+brew install autoconf
+brew install automake
+brew install libtool
+brew install cmake
+brew install glew
+brew install hdf5
+
+ln -s /usr/local/Cellar/boost-python/1.72.0/lib/libboost_python27.dylib /usr/local/lib/libboost_python.dylib
+ln -s /usr/local/Cellar/boost-python/1.72.0/lib/libboost_python27.a /usr/local/lib/libboost_python.a
+ln -s /usr/local/Cellar/boost-python/1.72.0/lib/libboost_python27-mt.dylib /usr/local/lib/libboost_python-mt.dylib
+ln -s /usr/local/Cellar/boost-python/1.72.0/lib/libboost_python27-mt.a /usr/local/lib/libboost_python-mt.a
+
+pip install --upgrade pip setuptools
+pip install numpy
+
+git clone https://github.com/openexr/openexr
+cd openexr/IlmBase
+./bootstrap
+./configure
+make; make install
+cd ../PyIlmBase
+./bootstrap
+./configure
+make; make install
+
+cd ~./code
+git clone https://github.com/alembic/alembic
+cd ./alembic; mkdir ./build; cd ./build
+
+cmake ../ \
+-DUSE_PYALEMBIC=ON \
+-DCMAKE_CXX_STANDARD=11 \
+-DCXX_STANDARD=11 \
+-DCMAKE_BUILD_TYPE=Release \
+-DALEMBIC_LIB_USES_BOOST=ON \
+-DPYTHON_EXECUTABLE=/usr/local/bin/python2 \
+-DPYTHON_INCLUDE_DIR=/usr/local/Cellar/python@2/2.7.17_1/Frameworks/Python.framework/Versions/2.7/include/python2.7 \
+-DPYTHON_LIBRARY=/usr/local/Cellar/python@2/2.7.17_1/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib \
+-DPYTHON_LIBRARY_DEBUG=/usr/local/Cellar/python@2/2.7.17_1/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib
+make; make install;
+
+
+brew install ffmpeg
+brew install freetype
+brew install giflib
+brew install jpeg
+brew install jpeg-turbo
+brew install libpng
+brew install libraw
+brew install libtiff
+brew install opencolorio
+brew install webp
+brew install ptex
+brew install opencv
+brew install openvdb
+brew install libheif
+brew install pybind11
+brew install libsquish
+
+cd ~./code
+git clone https://github.com/openimageio/oiio
+cd ./oiio
+mkdir ./build
+cd ./build
+cmake ../ \
+-DPYTHON_EXECUTABLE=/usr/local/bin/python2 \
+-DPYTHON_INCLUDE_DIR=/usr/local/Cellar/python@2/2.7.17_1/Frameworks/Python.framework/Versions/2.7/include/python2.7 \
+-DPYTHON_LIBRARY=/usr/local/Cellar/python@2/2.7.17_1/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib \
+-DPYTHON_LIBRARY_DEBUG=/usr/local/Cellar/python@2/2.7.17_1/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib \
+-DUSE_QT=OFF \
+-DUSE_PYTHON=ON
+
+make; make install
+
+brew install qt5
+pip install pyside2
+pip install psutil
+pip install slackclient
---
+++ build-scripts/osx/make-package.sh
@@ -0,0 +1,200 @@
+# Bookmarks library packager.
+#
+# The script copies all the dependencies to needed to run Bookmarks to the $DEST
+# folder. Starting from $TOPLIBS the script copies recursively all dependencies
+# and changes the linking settings using install_name_tool
+#
+
+clear && printf "\x1B[3J"
+
+LIB_SSL=/usr/local/Cellar/openssl@1.1/1.1.1d/lib/libssl.1.1.dylib
+LIB_CRYPTO=/usr/local/Cellar/openssl@1.1/1.1.1d/lib/libcrypto.1.1.dylib
+LIB_ALEMBIC=/usr/local/lib/python2.7/site-packages/alembic.so
+LIB_ALEMBIC_D=/usr/local/lib/libAlembic.1.7.12.dylib
+LIB_IEX=/usr/local/lib/python2.7/site-packages/iexmodule.so
+LIB_IMATH=/usr/local/lib/python2.7/site-packages/imathmodule.so
+LIB_OIIO=/usr/local/lib/python2.7/site-packages/OpenImageIO.so
+LIB_OIIO_D=/usr/local/lib/libOpenImageIO.2.2.2.dylib
+LIB_PYTHON=/usr/local/Cellar/python@2/2.7.17_1/Frameworks/Python.framework/Versions/2.7/Python
+DIR_PYTHON=/usr/local/Cellar/python@2/2.7.17_1/Frameworks/Python.framework/Versions/2.7/lib/python2.7
+PYTHON_BIN=/usr/local/Cellar/python@2/2.7.17_1/Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python
+
+TOPLIBS=(\
+[0]=$LIB_OIIO \
+[1]=$LIB_OIIO_D \
+[2]=$LIB_ALEMBIC_D \
+[3]=$LIB_ALEMBIC \
+[5]=$LIB_IEX \
+[6]=$LIB_IMATH \
+[7]=$LIB_SSL \
+[8]=$LIB_CRYPTO \
+);
+
+MODULE_ROOT=~/code/gwbrowser/gwbrowser
+PRODUCT=Bookmarks
+ROOT=~/code/$PRODUCT/Contents
+RES=$ROOT/Resources
+DEST=$RES/lib
+
+make_dirs() {
+	mkdir "./$PRODUCT" # Top
+	mkdir "$ROOT"
+	touch "$ROOT/Info.plist"
+	mkdir "$ROOT/MacOS"
+	mkdir "$RES"
+	mkdir "$DEST"
+}
+
+
+change_lib() {
+	echo -e "\x1B[4;34m\n$name:\x1B[0m"
+	name=$(basename "$1")
+	dir=$(dirname "$1")
+	otool -L $1 | grep -o -i -E "[\@\/].*?\s" | while read line;
+	do
+		name=$(basename "$line")
+		case $line in */usr/lib*) continue;; esac
+		case $line in */System/*) continue;; esac
+
+		case $line in */Python*)
+			name="libpython2.7.dylib";;
+		esac
+
+		echo -e "\x1B[39m$line\x1B[0m  ->  \x1B[32m@executable_path/lib/$name\x1B[0m"
+		sudo install_name_tool -change $line "@executable_path/lib/$name" "$1"
+		sudo install_name_tool -id @loader_path/$name "$1"
+	done
+}
+
+otool_copy() {
+	_name=$(basename "$1")
+	if test -f "$DEST/$_name"; then
+		:
+	else
+		if test -f "$1"; then
+			cp "$1" "$DEST/$_name"
+			EXITCODE=$?
+			test $EXITCODE -eq 0 && echo -e "\x1B[32mCopied "$1" \x1B[0m\n"
+		else
+			echo -e "\x1B[31m$1 does not exist\x1B[0m\n"
+		fi
+	fi
+
+	otool -L "$1" | grep -o -i -E "[\@\/].*?\s" | while read line;
+  do
+		name=$(basename "$line")
+		if test -f "$DEST/$name"; then
+			continue;
+		fi
+
+		# Skip built-ins and relative references
+    case "$line" in */usr/lib*) continue;; esac
+		case "$line" in */System/*) continue;; esac
+		case "$line" in *@*) continue;; esac
+
+		otool_copy $line
+		if test -f "$DEST/$_name"; then
+			sudo install_name_tool -id "@loader_path/$name" "$DEST/$_name";
+
+			if test -f "$DEST/$name"; then
+				sudo install_name_tool -change $line @executable_path/lib/$name "$DEST/$_name";
+			else
+				echo -e "\x1B[31minstall_name_tool: $name does not exist \x1B[0m\n"
+
+			fi
+		else
+				echo -e "\x1B[31minstall_name_tool: $DEST/$_name does not exist \x1B[0m\n"
+		fi
+
+	done
+}
+
+
+copy_python() {
+	cp "$LIB_PYTHON" "$DEST/libpython2.7.dylib"
+	EXITCODE=$?
+	test $EXITCODE -eq 0 && echo -e "\x1B[32mCopied $LIB_PYTHON \x1B[0m"
+
+	cp -r "$DIR_PYTHON" "$DEST"
+	test $EXITCODE -eq 0 && echo -e "\x1B[32mCopied $DIR_PYTHON \x1B[0m"
+	SITE_PACKAGES=$DEST/python2.7/site-packages
+
+	rm -r "$SITE_PACKAGES"
+	mkdir "$SITE_PACKAGES"
+
+	cp -r "$DIR_PYTHON/site-packages/PySide2" "$SITE_PACKAGES/PySide2"
+	test $EXITCODE -eq 0 && echo -e "\x1B[32mCopied $SITE_PACKAGES/PySide2 \x1B[0"
+	cp -r "$DIR_PYTHON/site-packages/shiboken2" "$SITE_PACKAGES/shiboken2"
+	test $EXITCODE -eq 0 && echo -e "\x1B[32mCopied $SITE_PACKAGES/shiboken2 \x1B[0m"
+	cp -r "$DIR_PYTHON/site-packages/numpy" "$SITE_PACKAGES/numpy"
+	test $EXITCODE -eq 0 && echo -e "\x1B[32mCopied $SITE_PACKAGES/numpy \x1B[0m"
+	cp -r "$DIR_PYTHON/site-packages/psutil" "$SITE_PACKAGES/psutil"
+	test $EXITCODE -eq 0 && echo -e "\x1B[32mCopied $SITE_PACKAGES/psutil \x1B[0m"
+	cp -r "$DIR_PYTHON/site-packages/slackclient" "$SITE_PACKAGES/slackclient"
+	test $EXITCODE -eq 0 && echo -e "\x1B[32mCopied $SITE_PACKAGES/slackclient \x1B[0m"
+	cp -r "$DIR_PYTHON/site-packages/requests" "$SITE_PACKAGES/requests"
+	test $EXITCODE -eq 0 && echo -e "\x1B[32mCopied $SITE_PACKAGES/requests \x1B[0m"
+	cp -r "$DIR_PYTHON/site-packages/urllib3" "$SITE_PACKAGES/urllib3"
+	test $EXITCODE -eq 0 && echo -e "\x1B[32mCopied $SITE_PACKAGES/urllib3 \x1B[0m"
+	cp -r "$DIR_PYTHON/site-packages/idna" "$SITE_PACKAGES/idna"
+	test $EXITCODE -eq 0 && echo -e "\x1B[32mCopied $SITE_PACKAGES/idna \x1B[0m"
+	cp -r "$DIR_PYTHON/site-packages/chardet" "$SITE_PACKAGES/chardet"
+	test $EXITCODE -eq 0 && echo -e "\x1B[32mCopied $SITE_PACKAGES/chardet \x1B[0m"
+	cp -r "$DIR_PYTHON/site-packages/certifi" "$SITE_PACKAGES/certifi"
+	test $EXITCODE -eq 0 && echo -e "\x1B[32mCopied $SITE_PACKAGES/certifi \x1B[0m"
+	cp -r "$DIR_PYTHON/site-packages/websocket-client" "$SITE_PACKAGES/websocket-client"
+	test $EXITCODE -eq 0 && echo -e "\x1B[32mCopied $SITE_PACKAGES/websocket-client \x1B[0m"
+	cp -r "$DIR_PYTHON/site-packages/websocket" "$SITE_PACKAGES/websocket"
+	test $EXITCODE -eq 0 && echo -e "\x1B[32mCopied $SITE_PACKAGES/websocket \x1B[0m"
+	cp -r "$DIR_PYTHON/site-packages/six.py" "$SITE_PACKAGES/six.py"
+	test $EXITCODE -eq 0 && echo -e "\x1B[32mCopied $SITE_PACKAGES/six.py \x1B[0m"
+
+	cp "$PYTHON_BIN" "$RES/Python"
+	EXITCODE=$?
+	test $EXITCODE -eq 0 && echo -e "\x1B[32mCopied $RES/Python \x1B[0m"
+	otool -L "$RES/Python" | grep -o -i -E "[\@\/].*?\s" | while read line;
+  do
+		name=$(basename "$line")
+		# Skip built-ins and relative references
+    case "$line" in */usr/lib*) continue;; esac
+		case "$line" in */System/*) continue;; esac
+		echo "$line"
+		sudo install_name_tool -change $line "@executable_path/lib/libpython2.7.dylib" "$RES/Python"
+		sudo install_name_tool -id "@loader_path/lib/libpython2.7.dylib" "$RES/Python"
+	done
+
+	for i in $DEST/*.so;
+	do
+		mv "$i" "$SITE_PACKAGES/"
+	done
+
+	cp -r "$MODULE_ROOT" "$SITE_PACKAGES"
+}
+
+test_install() {
+	"$RES/Python" -c "import imath; print 'imath -- \x1B[32mok\x1B[0m'"
+	"$RES/Python" -c "import iex; print 'iex -- \x1B[32mok\x1B[0m'"
+	"$RES/Python" -c "import alembic; print 'alembic -- \x1B[32mok\x1B[0m'"
+	"$RES/Python" -c "import OpenImageIO; print 'OpenImageIO -- \x1B[32mok\x1B[0m'"
+	"$RES/Python" -c "import gwbrowser; print 'gwbrowser -- \x1B[32mok\x1B[0m'"
+	"$RES/Python" -c "import slackclient; print 'slackclient -- \x1B[32mok\x1B[0m'"
+	"$RES/Python" -c "from PySide2 import QtCore, QtWidgets, QtGui; print 'PySide2 -- \x1B[32mok\x1B[0m'"
+	"$RES/Python" $MODULE_ROOT/../test/test_application.py
+}
+
+# Create directory structure
+make_dirs
+for i in "${TOPLIBS[@]}"
+do
+	# Copy recursively all dependent libraries and change linking
+	otool_copy $i
+done
+
+for i in $DEST/*;
+do
+	sudo chmod 777 "$i"
+	change_lib $i
+done
+
+copy_python
+test_install
