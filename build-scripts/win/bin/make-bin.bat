92715d884108d7c870bc9ab596a75f8ab9eb99d0
Gergely Wootsch <hello@gergely-wootsch.com>
2020-03-14 23:28:11 +0100

    Add automated packaging for windows

---
+++ build-scripts/win/bin/make-bin.bat
@@ -0,0 +1,63 @@
+@echo off
+
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+
+REM Builds bookmarks.exe
+REM Requires Visual Studio 2015 and Python 2.7
+
+set PRODUCT=bookmarks
+set PYTHON_INCLUDE="C:\Python27\Include"
+set PYTHON_LIB="C:\Python27\libs\python27.lib"
+
+REM Compile the cpp
+cl /c /EHsc /nologo /GS /GL /W3 /Gy /Zc:wchar_t ^
+/I%PYTHON_INCLUDE% ^
+/Zi /Gm- /O2 /sdl ^
+/Fd"%BUILD_DIR%\vc140.pdb" /Zc:inline /fp:precise ^
+/D "NDEBUG" /D "_CONSOLE" /D "_UNICODE" /D "UNICODE" ^
+/errorReport:prompt /WX- /Zc:forScope /Gd /Oi /MD ^
+%PRODUCT%.cpp
+
+REM Compile the resource file for showing the icon
+rc bookmarks.rc
+IF %ERRORLEVEL% NEQ 0 (
+  EXIT 1
+)
+
+REM Build the Debug Exe
+link ^
+/OUT:"%PRODUCT%_d.exe" ^
+/MANIFEST /LTCG:incremental /NXCOMPAT ^
+/DYNAMICBASE "%PYTHON_LIB%" "Shlwapi.lib" "kernel32.lib" "user32.lib" "gdi32.lib" "winspool.lib" "comdlg32.lib" "advapi32.lib" "shell32.lib" "ole32.lib" "oleaut32.lib" "uuid.lib" "odbc32.lib" "odbccp32.lib" ^
+/DEBUG /MACHINE:X64 /OPT:REF /INCREMENTAL:NO ^
+/SUBSYSTEM:CONSOLE /MANIFESTUAC:"level='asInvoker' uiAccess='false'" ^
+/OPT:ICF /ERRORREPORT:PROMPT /NOLOGO /TLBID:1 ^
+%PRODUCT%.obj %PRODUCT%.res
+
+IF %ERRORLEVEL% NEQ 0 (
+  EXIT 1
+)
+
+REM Build the Release Exe
+link ^
+/OUT:"%PRODUCT%.exe" ^
+/MANIFEST /LTCG:incremental /NXCOMPAT ^
+/DYNAMICBASE "%PYTHON_LIB%" "Shlwapi.lib" "kernel32.lib" "user32.lib" "gdi32.lib" "winspool.lib" "comdlg32.lib" "advapi32.lib" "shell32.lib" "ole32.lib" "oleaut32.lib" "uuid.lib" "odbc32.lib" "odbccp32.lib" ^
+/DEBUG /MACHINE:X64 /OPT:REF /INCREMENTAL:NO ^
+/SUBSYSTEM:CONSOLE /MANIFESTUAC:"level='asInvoker' uiAccess='false'" ^
+/OPT:ICF /ERRORREPORT:PROMPT /NOLOGO /TLBID:1 ^
+/SUBSYSTEM:windows /ENTRY:mainCRTStartup ^
+%PRODUCT%.obj %PRODUCT%.res
+
+IF %ERRORLEVEL% NEQ 0 (
+  EXIT 1
+)
+
+del /q /f *.manifest
+del /q /f *.iobj
+del /q /f *.obj
+del /q /f *.ipdb
+del /q /f *.pdb
+del /q /f *.res
+
+EXIT 0
--- build-scripts/win/make-package.py
+++ build-scripts/win/make-package.py
@@ -1,83 +1,212 @@
-"""There currently no automated packaging set up.
+""" Windows Packager script for Bookmarks.
 
-I am using
+The script build the Bookmarks.exe binary and collects the lkibrary dependencies
+needed to run the script.
+
+Modify the *_ROOT variables to point to the respective dependencie's install
+directory.
+
+NOTE: There's no automated build script for the dependencies at the moment, so
+make sure you're linking against the same includes and libraries as used for
+building ALembic and OpenImageIO.
 
 """
 
 import os
 import sys
+import shutil
+import zipfile
+from distutils.dir_util import copy_tree
 
-DUMPBIN = ur'"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64\dumpbin.exe"'
-PACKAGE_ROOT = ur'I:\dev\bookmarks-standalone\standalone'
 
+VC_ROOT = ur'C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64'
+DUMPBIN = ur'"' + VC_ROOT + os.path.sep + ur'dumpbin.exe"'
 ALEMBIC_ROOT = ur'I:\dev\alembic-build\install'
 OIIO_ROOT = ur'I:\dev\OpenImageIO-build\install'
 OPENEXR_ROOT = ur'I:\dev\openexr-build\install'
 OPENVDB_ROOT = ur'I:\dev\openvdb-build\install'
+QT5_ROOT = ur'I:\dev\qt5_13_2_build'
+PYSIDE2_ROOT = ur'I:\dev\pyside-setup\pyside2_install\py2.7-qt5.13.2-64bit-release'
 HEIF_ROOT = ur'I:\dev\libheif-build\libheif\Release'
 VCPKG_BIN = ur'I:\dev\vcpkg\installed\x64-windows\bin'
 PYTHON_ROOT = ur'C:\Python27'
 
+
+PACKAGE_ROOT = os.path.dirname(__file__) + ur'\..\..\..\bookmarks-standalone'
+PACKAGE_ROOT = os.path.normpath(PACKAGE_ROOT)
+
+
 LIB_ALEMBIC = ALEMBIC_ROOT + os.path.sep + 'lib' + os.path.sep + 'Alembic.dll'
-LIB_ALEMBIC_D = ALEMBIC_ROOT + os.path.sep + 'site-packages' + os.path.sep + 'alembic.pyd'
-LIB_OIIO =  OIIO_ROOT + os.path.sep + 'bin' + os.path.sep + 'OpenImageIO.dll'
-LIB_OIIO_D =  OIIO_ROOT + os.path.sep + 'lib' + os.path.sep + 'python2.7' + os.path.sep + 'site-packages' + os.path.sep + 'OpenImageIO.pyd'
-LIB_OPENVDB =  OPENVDB_ROOT + os.path.sep + 'bin' + os.path.sep + 'openvdb.dll'
+LIB_ALEMBIC_D = ALEMBIC_ROOT + os.path.sep + \
+    'site-packages' + os.path.sep + 'alembic.pyd'
+
+LIB_ILMIMF = OPENEXR_ROOT + os.path.sep + \
+    'bin' + os.path.sep + 'IlmImf-2_4.dll'
+LIB_IEX = OPENEXR_ROOT + os.path.sep + 'bin' + \
+    os.path.sep + 'PyIex_Python2_7-2_4.dll'
+LIB_IMATH = OPENEXR_ROOT + os.path.sep + 'bin' + \
+    os.path.sep + 'PyImath_Python2_7-2_4.dll'
+LIB_IEX_D = OPENEXR_ROOT + os.path.sep + 'lib' + os.path.sep + \
+    'site-packages' + os.path.sep + 'iex.pyd'
+LIB_IMATH_D = OPENEXR_ROOT + os.path.sep + 'lib' + os.path.sep + \
+    'site-packages' + os.path.sep + 'imath.pyd'
+
+LIB_OIIO = OIIO_ROOT + os.path.sep + 'bin' + os.path.sep + 'OpenImageIO.dll'
+LIB_OIIO_D = OIIO_ROOT + os.path.sep + 'lib' + os.path.sep + 'python2.7' + \
+    os.path.sep + 'site-packages' + os.path.sep + 'OpenImageIO.pyd'
+LIB_OPENVDB = OPENVDB_ROOT + os.path.sep + 'bin' + os.path.sep + 'openvdb.dll'
+
+
+LIB_QTCORE = QT5_ROOT + os.path.sep + 'bin' + os.path.sep + 'Qt5Core.dll'
+LIB_QTGUI = QT5_ROOT + os.path.sep + 'bin' + os.path.sep + 'Qt5Gui.dll'
+LIB_QTWIDGETS = QT5_ROOT + os.path.sep + 'bin' + os.path.sep + 'Qt5Widgets.dll'
+LIB_QTCORE_D = PYSIDE2_ROOT + os.path.sep + 'lib' + os.path.sep + \
+    'site-packages' + os.path.sep + 'PySide2' + os.path.sep + 'QtCore.pyd'
+LIB_QTGUI_D = PYSIDE2_ROOT + os.path.sep + 'lib' + os.path.sep + \
+    'site-packages' + os.path.sep + 'PySide2' + os.path.sep + 'QtGui.pyd'
+LIB_QTWIDGETS_D = PYSIDE2_ROOT + os.path.sep + 'lib' + os.path.sep + \
+    'site-packages' + os.path.sep + 'PySide2' + os.path.sep + 'QtWidgets.pyd'
+
+LIB_SHIBOKEN = PYSIDE2_ROOT + os.path.sep + \
+    'bin' + os.path.sep + 'shiboken2-python2.7.dll'
+LIB_SHIBOKEN_D = PYSIDE2_ROOT + os.path.sep + 'lib' + os.path.sep + \
+    'site-packages' + os.path.sep + 'shiboken2' + os.path.sep + 'shiboken2.pyd'
+# LIB_EGL = QT5_ROOT + os.path.sep + 'bin' + os.path.sep + 'libEGL.dll'
+# LIB_GLES = QT5_ROOT + os.path.sep + 'bin' + os.path.sep + 'libGLESv2.dll'
 
 
 TOPLIBS = (
+    LIB_ILMIMF,
+    LIB_IEX,
+    LIB_IEX_D,
+    LIB_IMATH,
+    LIB_IMATH_D,
     LIB_ALEMBIC,
     LIB_ALEMBIC_D,
     LIB_OIIO,
     LIB_OIIO_D,
     LIB_OPENVDB,
-    # LIB_ALEMBIC_D,
-    # LIB_ALEMBIC,
-    # LIB_IEX,
-    # LIB_IMATH,
-    # LIB_SSL,
-    # LIB_CRYPTO
+    LIB_QTCORE,
+    LIB_QTCORE_D,
+    LIB_QTGUI,
+    LIB_QTGUI_D,
+    LIB_QTWIDGETS,
+    LIB_QTWIDGETS_D,
+    LIB_SHIBOKEN,
+    LIB_SHIBOKEN_D,
 )
+
 
 for l in TOPLIBS:
     if not os.path.isfile(l):
         raise OSError('{} does not exists'.format(l))
 
+
+def find_lib(lib):
+    if os.path.isfile(lib):
+        return lib
+
+    for _ROOT in (
+        ALEMBIC_ROOT,
+        OIIO_ROOT,
+        OPENEXR_ROOT,
+        HEIF_ROOT,
+        OPENVDB_ROOT,
+        HEIF_ROOT,
+        VCPKG_BIN,
+        QT5_ROOT,
+        PYSIDE2_ROOT,
+        PYTHON_ROOT,
+    ):
+        for root, _, files in os.walk(_ROOT):
+            for f in files:
+                if f.lower() == lib.lower():
+                    return root + os.path.sep + f
+    return None
+
+
 def get_dependencies():
-    def _get_libs(libpath, l):
+    builtins = (
+        'ADVAPI32',
+        'concrt140',
+        'GDI32',
+        'KERNEL',
+        'MF',
+        'MFPlat',
+        'MPR',
+        'MSVCP',
+        'NETAPI32',
+        'OLEAUT32',
+        'OPENGL32',
+        'SHELL32',
+        'SHLWAPI',
+        'Secur32',
+        'USER32',
+        'USERENV',
+        'UxTheme',
+        'VCOMP140',
+        'VCRUNTIME',
+        'VERSION',
+        'WINMM',
+        'WS2_32',
+        'api-ms',
+        'bcrypt',
+        'd3d11',
+        'dbeng',
+        'dbgeng',
+        'dwmapi',
+        'ole32',
+        'ucrtbased',
+    )
+
+    def get_lib_dependencies(libpath, l):
+        libpath = os.path.basename(libpath)
+        _libpath = find_lib(libpath)
+        if not _libpath:
+            raise RuntimeError('Could not find {}'.format(libpath))
+        else:
+            libpath = _libpath
+
         DUMPCMD = '{} /dependents {}'.format(DUMPBIN, libpath)
         r = os.popen(DUMPCMD).read()
         r = r.strip().split('\n')
-        _dep = [f.strip() for f in r if f.lower().endswith('dll') and not f.lower().startswith('dump')]
+        _dep = [f.strip() for f in r if f.lower().endswith(
+            'dll') and not f.lower().startswith('dump')]
         _dep = [f for f in _dep if not f.lower().startswith('file type')]
-        _dep = [f for f in _dep if not [s for s in builtins if s.lower() in f.lower()]]
+        _dep = [f for f in _dep if not [
+            s for s in builtins if s.lower() in f.lower()]]
         l = sorted(list(set(l)))
         if not _dep:
             return l
         l += _dep
         for libpath in _dep:
-            l = _get_libs(libpath, l)
+            l = get_lib_dependencies(libpath, l)
         l = sorted(list(set(l)))
         return l
 
-    builtins = ('ADVAPI32', 'dbgeng', 'MSVCP', 'KERNEL', 'VCRUNTIME', 'api-ms', 'SHELL32', 'WS2_32', 'dbeng', 'ole32')
-
     dependencies = []
     for L in TOPLIBS:
-        dependencies = _get_libs(L, dependencies)
+        if L.endswith('dll'):
+            dependencies.append(L)
+        dependencies = get_lib_dependencies(L, dependencies)
 
     LIBS = {}
     for lib in dependencies:
         LIBS[lib] = None
+        if os.path.isfile(lib):
+            LIBS[lib] = lib
+            continue
         for _ROOT in (
             ALEMBIC_ROOT,
             OIIO_ROOT,
             OPENEXR_ROOT,
             HEIF_ROOT,
             OPENVDB_ROOT,
-            PYTHON_ROOT,
             HEIF_ROOT,
             VCPKG_BIN,
+            QT5_ROOT,
+            PYSIDE2_ROOT,
+            PYTHON_ROOT,
         ):
             for root, _, files in os.walk(_ROOT):
                 for f in files:
@@ -87,6 +216,7 @@ def get_dependencies():
             if LIBS[lib]:
                 break
 
+    # Pretty print with colors and all
     for k, v in sorted(LIBS.items(), key=lambda x: x[0]):
         if v is None:
             print '\x1B[37m\n', k, '\x1B[0m\n', '\x1B[31mNot Found\x1B[0m'
@@ -98,7 +228,173 @@ def get_dependencies():
             ','.join([i[0] for i in LIBS.items() if i[1] is None])
         ))
 
+    return LIBS
 
 
+def make_folders():
+    if not os.path.isdir(PACKAGE_ROOT):
+        os.mkdir(PACKAGE_ROOT)
 
-get_dependencies()
+    root = PACKAGE_ROOT + os.path.sep + u'bookmarks'
+    if not os.path.isdir(root):
+        os.mkdir(root)
+
+    if not os.path.isdir(root + os.path.sep + u'bin'):
+        os.mkdir(root + os.path.sep + u'bin')
+    if not os.path.isdir(root + os.path.sep + u'lib'):
+        os.mkdir(root + os.path.sep + u'lib')
+    if not os.path.isdir(root + os.path.sep + u'lib' + os.path.sep + 'site-packages'):
+        os.mkdir(root + os.path.sep + u'lib' + os.path.sep + 'site-packages')
+    if not os.path.isdir(root + os.path.sep + u'lib' + os.path.sep + 'site-packages' + os.path.sep + 'PySide2'):
+        os.mkdir(root + os.path.sep + u'lib' + os.path.sep + 'site-packages' + os.path.sep + 'PySide2')
+    if not os.path.isdir(root + os.path.sep + u'lib' + os.path.sep + 'site-packages' + os.path.sep + 'shiboken2'):
+        os.mkdir(root + os.path.sep + u'lib' + os.path.sep + 'site-packages' + os.path.sep + 'shiboken2')
+    if not os.path.isdir(root + os.path.sep + u'platforms'):
+        os.mkdir(root + os.path.sep + u'platforms')
+    if not os.path.isdir(root + os.path.sep + u'shared'):
+        os.mkdir(root + os.path.sep + u'shared')
+
+
+def copy_libs(libs):
+    root = PACKAGE_ROOT + os.path.sep + u'bookmarks'
+
+    for k in libs:
+        dest = root + os.path.sep + 'bin' + \
+            os.path.sep + os.path.basename(libs[k])
+        shutil.copy2(
+            libs[k], dest)
+        print libs[k], '>', dest
+
+    pzip = os.path.dirname(__file__) + os.path.sep + 'python27.zip'
+    if not os.path.isfile(pzip):
+        raise RuntimeError('Python module zip is missing')
+
+    with zipfile.ZipFile(pzip, 'r') as zip:
+        zip.extractall(path=root + os.path.sep + 'lib')
+        print pzip, '>', root + os.path.sep + 'lib'
+
+    for k in (
+        LIB_ALEMBIC_D,
+        LIB_IEX_D,
+        LIB_IMATH_D,
+    ):
+        shutil.copy2(
+            k,
+            root + os.path.sep + 'lib' + os.path.sep + 'site-packages' + os.path.sep + os.path.basename(k))
+
+    shutil.copy2(
+        LIB_QTCORE_D,
+        root + os.path.sep + u'lib' + os.path.sep + 'site-packages' + os.path.sep + 'PySide2' + os.path.sep + os.path.basename(LIB_QTCORE_D))
+    shutil.copy2(
+        LIB_QTGUI_D,
+        root + os.path.sep + u'lib' + os.path.sep + 'site-packages' + os.path.sep + 'PySide2' + os.path.sep + os.path.basename(LIB_QTGUI_D))
+    shutil.copy2(
+        LIB_QTWIDGETS_D,
+        root + os.path.sep + u'lib' + os.path.sep + 'site-packages' + os.path.sep + 'PySide2' + os.path.sep + os.path.basename(LIB_QTWIDGETS_D))
+
+    # PySide2
+    pyside_root = PYSIDE2_ROOT + os.path.sep + 'lib' + \
+        os.path.sep + 'site-packages' + os.path.sep + 'PySide2'
+    shutil.copy2(
+        pyside_root + os.path.sep + '__init__.py',
+        root + os.path.sep + u'lib' + os.path.sep + 'site-packages' + os.path.sep + 'PySide2' + os.path.sep + '__init__.py')
+
+
+    # Shiboken
+    shiboken_root = PYSIDE2_ROOT + os.path.sep + 'lib' + \
+        os.path.sep + 'site-packages' + os.path.sep + 'shiboken2'
+    shutil.copy2(
+        shiboken_root + os.path.sep + 'shiboken2.pyd',
+        root + os.path.sep + u'lib' + os.path.sep + 'site-packages' + os.path.sep + 'shiboken2' + os.path.sep + 'shiboken2.pyd')
+    shutil.copy2(
+        shiboken_root + os.path.sep + '__init__.py',
+        root + os.path.sep + u'lib' + os.path.sep + 'site-packages' + os.path.sep + 'shiboken2' + os.path.sep + '__init__.py')
+    copy_tree(
+        shiboken_root + os.path.sep + 'files.dir',
+        root + os.path.sep + u'lib' + os.path.sep + 'site-packages' + os.path.sep + 'shiboken2' + os.path.sep + 'files.dir'
+    )
+
+    shutil.copy2(
+        QT5_ROOT + os.path.sep + 'plugins' + os.path.sep + 'platforms' + os.path.sep + 'qwindows.dll',
+        root + os.path.sep + u'platforms' + os.path.sep + 'qwindows.dll')
+
+    shutil.copy2(
+        LIB_OIIO_D,
+        root + os.path.sep + 'shared' + os.path.sep + os.path.basename(LIB_OIIO_D))
+
+
+def build_bin():
+    bindir = os.path.normpath(
+        __file__ + os.path.sep + '..' + os.path.sep + 'bin')
+    os.chdir(bindir)
+    os.system(bindir + os.path.sep + 'make-bin.bat')
+    if not os.path.isfile(bindir + os.path.sep + 'bookmarks.exe'):
+        raise RuntimeError('bookmarks.exe not found')
+    if not os.path.isfile(bindir + os.path.sep + 'bookmarks_d.exe'):
+        raise RuntimeError('bookmarks_d.exe not found')
+    shutil.copy2(
+        bindir + os.path.sep + 'bookmarks.exe',
+        PACKAGE_ROOT + os.path.sep + u'bookmarks' + os.path.sep + 'bookmarks.exe')
+    shutil.copy2(
+        bindir + os.path.sep + 'bookmarks_d.exe',
+        PACKAGE_ROOT + os.path.sep + u'bookmarks' + os.path.sep + 'bookmarks_d.exe')
+    shutil.move(
+        PACKAGE_ROOT + os.path.sep + u'bookmarks' +
+        os.path.sep + 'bin' + os.path.sep + 'python27.dll',
+        PACKAGE_ROOT + os.path.sep + u'bookmarks' + os.path.sep + 'python27.dll',
+    )
+
+
+def install_python_modules():
+    root = PACKAGE_ROOT + os.path.sep + u'bookmarks'
+    pzip = os.path.dirname(__file__) + os.path.sep + 'numpy.zip'
+    if not os.path.isfile(pzip):
+        raise RuntimeError('numpy.zip is missing')
+    with zipfile.ZipFile(pzip, 'r') as zip:
+        zip.extractall(path=root + os.path.sep + 'shared')
+        print pzip, '>', root + os.path.sep + 'shared'
+
+    pzip = os.path.dirname(__file__) + os.path.sep + 'psutil.zip'
+    if not os.path.isfile(pzip):
+        raise RuntimeError('psutil.zip is missing')
+    with zipfile.ZipFile(pzip, 'r') as zip:
+        zip.extractall(path=root + os.path.sep + 'shared')
+        print pzip, '>', root + os.path.sep + 'shared'
+
+    source = os.path.normpath(__file__ + os.path.sep + '..' + os.path.sep +
+                              '..' + os.path.sep + '..' + os.path.sep + 'bookmarks')
+    if not os.path.isdir(source):
+        raise RuntimeError('bookmarks is missing.')
+    copy_tree(
+        source,
+        PACKAGE_ROOT + os.path.sep + u'bookmarks' +
+        os.path.sep + 'shared' + os.path.sep + 'bookmarks'
+    )
+
+    target = PACKAGE_ROOT + os.path.sep + u'bookmarks' + os.path.sep + 'shared'
+    cmd = ur'pip install --target="{}" --compile --no-cache-dir SlackClient'.format(
+        target)
+    os.system(cmd)
+
+    p = root + os.path.sep + 'shared'
+    for f in os.listdir(p):
+        if 'dist-info' in f:
+            shutil.rmtree(p + os.path.sep + f)
+
+def inno_setup():
+    INNO_COMPILER = ur'C:\Program Files (x86)\Inno Setup 6\iscc.exe'
+    if not os.path.isfile(INNO_COMPILER):
+        raise ValueError('Could not find Inno Setup')
+
+    p = os.path.dirname(__file__)
+    os.chdir(p)
+    cmd = u'"{}" .\\installer.iss'.format(INNO_COMPILER)
+    os.system(cmd)
+
+if __name__ == '__main__':
+    # make_folders()
+    # libs = get_dependencies()
+    # copy_libs(libs)
+    # build_bin()
+    # install_python_modules()
+    inno_setup()
