bf85d6ab132ae2949cf9630a5632ed1adb7e1ca3
Gergely Wootsch <hello@gergely-wootsch.com>
2020-06-28 16:23:30 +0200

    Bump version to 0.3.12

--- bookmarks/__init__.py
+++ bookmarks/__init__.py
@@ -11,7 +11,7 @@ name = u'bookmarks'
 author = 'Gergely Wootsch'
 website = 'https://gergely-wootsch.com'
 email = 'hello@gergely-wootsch.com'
-__version__ = u'0.3.11'
+__version__ = u'0.3.12'
 
 
 def get_info():
