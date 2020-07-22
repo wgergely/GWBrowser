72333b4ab22445f27b781643c33101316d4034b8
Gergely Wootsch <hello@gergely-wootsch.com>
2020-07-22 11:07:38 +0200

    Update Maya plugin imports
    Use `INSTALL_ROOT` env variable to find the python package instead of
    hard-coding its name.

--- bookmarks/maya/mBookmarks.py
+++ bookmarks/maya/mBookmarks.py
@@ -2,20 +2,21 @@
 """Bookmarks Maya plugin.
 
 Make sure Bookmark is installed before trying to load the plugin.
-The `BOOKMARKS_ROOT` environment is set by the installer and is
-required to find and load all necessary Python modules.
+The `*_ROOT` environment is set by the installer and is
+required to find and load all the necessary Python modules.
 
 """
 import sys
 import os
 
-from PySide2 import QtWidgets, QtCore
-
 from maya.app.general.mayaMixin import mixinWorkspaceControls
 import maya.api.OpenMaya as OpenMaya
 import maya.OpenMayaUI as OpenMayaUI
-
 import maya.cmds as cmds
+
+
+INSTALL_ROOT = 'BOOKMARKS_ROOT'
+pkg_name = INSTALL_ROOT.lower().replace('_root', '').strip()
 
 
 def maya_useNewAPI():
@@ -30,64 +31,69 @@ def maya_useNewAPI():
 def initializePlugin(plugin):
     """Loads the Bookmarks plugin.
 
-    Python libraries are found in the `%BOOKMARKS_ROOT%/shared` folder and the
-    binaries are in `%BOOKMARKS_ROOT%/bin`.
+    Python libraries are found in the `%*_ROOT%/shared` folder and the
+    binaries in `%*_ROOT%/bin`.
 
-    The environment is set by the installer normally and should point to the
+    The environment is set by the installer and should point to the
     folder where bookmarks.exe resides.
 
     """
-
-    k = 'BOOKMARKS_ROOT'
-    if k not in os.environ:
+    if INSTALL_ROOT not in os.environ:
         raise EnvironmentError(
-            u'Is Bookmarks installed?\n(Environment variable `{}` is not set.)'.format(k))
+            u'Is Bookmarks installed?\n(Environment variable `{}` is not set.)'.format(INSTALL_ROOT))
 
-    shared = u'{}{}{}'.format(os.environ[k], os.path.sep, u'shared')
-    bin = u'{}{}{}'.format(os.environ[k], os.path.sep, u'bin')
+    # Add the `shared` folder to the python path
+    shared = u'{}{}{}'.format(os.environ[INSTALL_ROOT], os.path.sep, u'shared')
     sys.path.insert(0, os.path.abspath(os.path.normpath(shared)))
+
+    # Add the `bin` folder to the current path envrionment
+    bin = u'{}{}{}'.format(os.environ[INSTALL_ROOT], os.path.sep, u'bin')
     path = u'{};{}'.format(
         os.path.abspath(os.path.normpath(bin)),
         os.environ['PATH'])
     os.environ['PATH'] = path
 
-    import bookmarks
+    try:
+        package = __import__(pkg_name)
+        common = __import__('{}.common'.format(pkg_name))
+        widget = __import__('{}.maya.widget'.format(pkg_name))
+    except:
+        raise
+
 
     pluginFn = OpenMaya.MFnPlugin(
-        plugin, vendor=u'Gergely Wootsch', version=bookmarks.__version__)
+        plugin, vendor=u'Gergely Wootsch', version=package.__version__)
 
     try:
-        import bookmarks.common as common
-
-        # Must initiate the font_db manually here.
+        # Initiate the font_db. This will load the used custom fonts to Maya.
         common.font_db = common.FontDatabase()
+
+        # Let Bookmarks know we're not launching it in standalone mode
         common.STANDALONE = False
 
-        # If Maya has any UI scaling, this will scale Bookmark's interface accordingly
+        # If Maya has any UI scaling set scale Bookmark's interface accordingly
         common.UI_SCALE = cmds.mayaDpiSetting(scaleValue=True, query=True)
 
-        import bookmarks.maya.widget as widget
+        # Load Bookmarks
         widget.maya_button = widget.MayaBrowserButton()
         cmds.evalDeferred(widget.maya_button.initialize)
-
-    except ImportError as e:
-        raise ImportError(e)
-
     except Exception as e:
         raise Exception(e)
 
 
 def uninitializePlugin(plugin):
     """Method is called by Maya when unloading the plug-in."""
-    import bookmarks
+    try:
+        package = __import__(pkg_name)
+        widget = __import__('{}.maya.widget'.format(pkg_name))
+    except ImportError as e:
+        raise 'Could not import \'{}\''.format(pkg_name)
+
     pluginFn = OpenMaya.MFnPlugin(
-        plugin, vendor=u'Gergely Wootsch', version=bookmarks.__version__)
+        plugin, vendor=u'Gergely Wootsch', version=package.__version__)
 
-    import bookmarks.maya as maya
-    widget = maya.widget.instance()
+    def done():
+        print '{} uninitialised.'.format(pkg_name)
 
-    def p():
-        print 'Plugin uninitialised.'
-
-    widget.terminated.connect(p)
-    widget.main.shutdown.emit()
+    widget.instance().terminated.connect(done)
+    widget.instance().main.shutdown.emit()
